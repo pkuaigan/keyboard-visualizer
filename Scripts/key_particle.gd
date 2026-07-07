e96361280be524ccc0228028d5fbf42a923a1f62
kuaiganDesktop <qizhou910@gmail.com>
2026-07-07 11:33:54 +0100

    flying particles ? maybe remove

--- Scripts/key_particle.gd
+++ Scripts/key_particle.gd
@@ -4,15 +4,17 @@ var radius := 0
 var time := 0.0
 var color := Color.WHITE
 var alpha := 1.0
+var direction : Vector2
 
 func setup(p_color: Color, intensity: float) -> void:
 	color = p_color
-	var tween := create_tween()
-	tween.set_parallel(true)
+	#var tween := create_tween()
+	#tween.set_parallel(true)
 	_get_properties(intensity)
-	tween.tween_method(_set_radius, 1.0, radius, time).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
-	tween.tween_method(_set_alpha, 1.0, 0.0, time).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
-	tween.chain().tween_callback(queue_free)
+	_fly_in_direction(250.0, time)
+	#tween.tween_method(_set_radius, 1.0, radius, time).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	#tween.tween_method(_set_alpha, 1.0, 0.0, time).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
+	#tween.chain().tween_callback(queue_free)
 
 func _set_radius(r: int) -> void:
 	radius = r
@@ -25,20 +27,37 @@ func _set_alpha(a: float) -> void:
 func _get_properties(intensity: float) -> void: # tweak
 	if intensity > 0.95: # maybe a skill issue but only get .95 when mashing keys vs actually typing so larger radius.... the small radius for mashing looks silly
 		radius = 200
-		time = 0.1
-	if intensity > 0.9:
+		time = 0.7
+	elif intensity > 0.9:
 		radius = 75
-		time = 0.3
+		time = 0.9
 	elif intensity > 0.8:
 		radius = 90
-		time = 0.5
+		time = 1.0
 	elif intensity > 0.75:
 		radius = 120
-		time = 0.7
+		time = 1.5
 	else:
 		radius = 150
-		time = 0.75
-		
+		time = 1.7
+
+func _get_direction() -> Vector2:
+	direction.x = randi_range(-10,10)
+	direction.y = randi_range(-10,10)
+	if direction.x != 0 and direction.y != 0:
+		return Vector2(direction.x, direction.y)
+	else:
+		return Vector2(5,1) # change
+	
+	
+func _fly_in_direction(speed: float, duration: float) -> void:
+	direction = _get_direction()
+	var distance = direction * speed
+	var tween := create_tween()
+	tween.tween_property(self, "position", distance, duration).as_relative()
+	tween.tween_method(_set_alpha, 1.0, 0.0, time).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
+	
+	queue_redraw()
 
 func _draw() -> void:
 	draw_circle(Vector2.ZERO, radius, Color(color.r, color.g, color.b, alpha))
